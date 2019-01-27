eb7a483cab17561b3e5edd3cb5c0adf783b89dc8
Matt Phillips <mattphillipsphd@gmail.com>
2019-01-26 20:03:38 -0500

    Added corresponding rudimentary Discriminator

--- 1_FundamentalsOfGANs/main.py
+++ 1_FundamentalsOfGANs/main.py
@@ -11,9 +11,7 @@ import torch.nn as nn
 import torch.utils.funcional as F
 
 class Generator(nn.Module):
-    def __init__(self):
-        super().__init__(z_dim=10)
-
+    def __init__(self, z_dim=10):
         self._fc1 = nn.Linear(z_dim, 20)
         self._fc2 = nn.Linear(20, 150)
         self._fc3 = nn.Linear(150, 784)
@@ -22,7 +20,20 @@ class Generator(nn.Module):
         x = F.relu( self._fc1(x) )
         x = F.relu( self._fc2(x) )
         x = F.sigmoid( self._fc3(x) )
+        x = x.view(-1, 28, 28)
         return x
 
 class Discriminator(nn.Module):
+    def __init__(self):
+        self._fc1 = nn.Linear(784, 150)
+        self._fc2 = nn.Linear(150, 20)
+        self._fc3 = nn.Linear(20, 1)
+
+    def forward(self, x):
+        x = x.view(-1, 784)
+        x = F.relu( self._fc1(x) )
+        x = F.relu( self._fc2(x) )
+        x = F.sigmoid( self._fc3(x) )
+        return x
+
 

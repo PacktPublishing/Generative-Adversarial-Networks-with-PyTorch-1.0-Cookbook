7e55a957c0754651ce2e9eec5477862473f75db9
Matt Phillips <mattphillipsphd@gmail.com>
2019-02-03 12:20:50 -0500

    WIP

--- 1_FundamentalsOfGANs/main.py
+++ 1_FundamentalsOfGANs/main.py
@@ -16,29 +16,41 @@ from torch.utils.data import DataLoader
 class Generator(nn.Module):
     def __init__(self, z_dim=10):
         super().__init__()
-        self._fc1 = nn.Linear(z_dim, 20)
-        self._fc2 = nn.Linear(20, 150)
-        self._fc3 = nn.Linear(150, 784)
+#        self._fc1 = nn.Linear(z_dim, 20)
+#        self._fc2 = nn.Linear(20, 150)
+#        self._fc3 = nn.Linear(150, 784)
+        self._fc1 = nn.Linear(z_dim, 150, bias=False)
+#        self._bn1 = nn.BatchNorm1d(150)
+        self._fc2 = nn.Linear(150, 784, bias=False)
+
+        nn.init.normal_(self._fc1.weight, mean=0, std=0.1)
+        nn.init.normal_(self._fc2.weight, mean=0, std=0.1)
 
     def forward(self, x):
         x = F.relu( self._fc1(x) )
-        x = F.relu( self._fc2(x) )
-        x = torch.sigmoid( self._fc3(x) )
+#        x = self._bn1(x)
+        x = torch.sigmoid( self._fc2(x) )
         x = x.view(-1, 1, 28, 28)
         return x
 
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
-        self._fc1 = nn.Linear(784, 150)
-        self._fc2 = nn.Linear(150, 20)
-        self._fc3 = nn.Linear(20, 1)
+        self._fc1 = nn.Linear(784, 150, bias=False)
+#        self._bn1 = nn.BatchNorm1d(150)
+        self._fc2 = nn.Linear(150, 1, bias=False)
+#        self._fc1 = nn.Linear(784, 150)
+#        self._fc2 = nn.Linear(150, 20)
+#        self._fc3 = nn.Linear(20, 1)
+
+        nn.init.normal_(self._fc1.weight, mean=0, std=0.1)
+        nn.init.normal_(self._fc2.weight, mean=0, std=0.1)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = F.relu( self._fc1(x) )
-        x = F.relu( self._fc2(x) )
-        x = torch.sigmoid( self._fc3(x) )
+#        x = self._bn1(x)
+        x = torch.sigmoid( self._fc2(x) )
         return x
 
 
@@ -57,62 +69,78 @@ def get_loaders(cfg):
             num_workers=cfg["num_workers"])
     return train_loader,test_loader
 
-def sample(m_gen, epoch, cfg):
-    cudev = cfg["cuda"]
-    if cudev >= 0:
-        z = torch.cuda.FloatTensor(cfg["batch_size"],
-                cfg["z_dim"]).uniform_(0.0,1.0)
-    else:
-        z = torch.FloatTensor(cfg["batch_size"], cfg["z_dim"]).uniform_(0.0,1.0)
+def save_sample_images(m_gen, epoch, cfg):
+    z = z_sampler(cfg["batch_size"], cfg["z_dim"], cfg["cuda"])
     xhat = m_gen(z)
     xhat = F.interpolate(xhat, scale_factor=(5.0, 5.0))
     tv.utils.save_image(xhat, "samples/%03d.png" % epoch)
 
-
-def train(m_gen, m_disc, train_loader, test_loader, optimizer, cfg):
+def train(m_gen, m_disc, train_loader, test_loader, optimizers, cfg):
     cudev = cfg["cuda"]
     batch_size = cfg["batch_size"]
+    optD,optG = optimizers
     d_criterion = lambda yhat,y : -torch.mean( y*torch.log(yhat) \
             + (1-y)*torch.log(1-yhat) )
     g_criterion = lambda yhat : -torch.mean( torch.log(yhat) )
+    criterion = nn.BCELoss()
     for epoch in range(100):
-        for i,(x,_) in enumerate(train_loader):
+        for i,(real_x,_) in enumerate(train_loader):
+            real_labels = torch.ones(batch_size).cuda(cudev)
+            fake_labels = torch.zeros(batch_size).cuda(cudev)
             if cudev >= 0:
-                z = torch.cuda.FloatTensor(batch_size,
-                        cfg["z_dim"]).uniform_(0.0,1.0)
-                x = x.cuda(cudev)
+                real_x = real_x.cuda(cudev)
+                real_labels = real_labels.cuda(cudev)
+                fake_labels = fake_labels.cuda(cudev)
                 labels = torch.cat((torch.ones(batch_size), 
                     torch.zeros(batch_size))).cuda(cudev)
-            else:
-                z = torch.FloatTensor(batch_size,cfg["z_dim"]).uniform_(0.0,1.0)
-                labels = torch.cat((torch.ones(batch_size), 
-                    torch.zeros(batch_size)))
-            
-            optimizer.zero_grad()
-            xhat = m_gen(z)
-            x = torch.cat((x, xhat))
-            preds = m_disc(x)
-            binary_preds = preds > 0.5
-            d_real_acc = 100.0 * torch.mean((binary_preds[:batch_size] \
-                    == labels[:batch_size].byte()).float())
-            d_fake_acc = 100.0 * torch.mean((binary_preds[batch_size:] \
-                    == labels[batch_size:].byte()).float())
-            d_real_preds = torch.mean(binary_preds[:batch_size].float())
-            d_fake_preds = torch.mean(binary_preds[batch_size:].float())
-            d_loss = d_criterion(preds, labels)
-            d_loss.backward(retain_graph=True)
+            z = z_sampler(batch_size, cfg["z_dim"], cudev)
 
-            optimizer.zero_grad()
-            g_loss = g_criterion(preds[batch_size:])
+            optD.zero_grad()
+            fake_x = m_gen(z)
+            d_fake_loss = criterion(m_disc(fake_x), fake_labels)
+            d_real_loss = criterion(m_disc(real_x), real_labels)
+            d_loss = d_fake_loss + d_real_loss
+
+            d_loss.backward()
+            optD.step()
+
+            optG.zero_grad()
+            fake_x2 = m_gen( z_sampler(batch_size, cfg["z_dim"], cudev) )
+            g_loss = criterion(m_disc(fake_x2), real_labels)
             g_loss.backward()
-            optimizer.step()
-            if i % 10 == 9:
-                print("LossG: %.4f, lossD: %.4f, Real accD: %.1f, " \
-                        "Fake accD: %.1f, Real preds: %.4f, Fake preds: %.4f" \
-                        %(d_loss.item(), g_loss.item(), d_real_acc, d_fake_acc,
-                            d_real_preds, d_fake_preds))
-        sample(m_gen, epoch, cfg)
+            optG.step()
 
+            print("GLoss: %.4f, DLossReal: %.4f, DLossFake: %.4f, " \
+                    % (g_loss.item(), d_real_loss.item(), d_fake_loss.item()))
+            
+#            preds = m_disc(x)
+#            binary_preds = preds > 0.5
+#            d_real_acc = 100.0 * torch.mean((binary_preds[:batch_size] \
+#                    == labels[:batch_size].byte()).float())
+#            d_fake_acc = 100.0 * torch.mean((binary_preds[batch_size:] \
+#                    == labels[batch_size:].byte()).float())
+#            d_real_preds = torch.mean(binary_preds[:batch_size].float())
+#            d_fake_preds = torch.mean(binary_preds[batch_size:].float())
+#            d_loss = d_criterion(preds, labels)
+#            d_loss.backward(retain_graph=True)
+#
+#            if i % 10 == 9:
+#                print("LossG: %.4f, lossD: %.4f, Real accD: %.1f, " \
+#                        "Fake accD: %.1f, Real preds: %.4f, Fake preds: %.4f" \
+#                        %(d_loss.item(), g_loss.item(), d_real_acc, d_fake_acc,
+#                            d_real_preds, d_fake_preds))
+        save_sample_images(m_gen, epoch, cfg)
+
+def z_sampler(batch_size, z_dim, cudev):
+#    if cudev >= 0:
+#        z = torch.cuda.FloatTensor(batch_size, z_dim).uniform_(0.0,1.0)
+#    else:
+#        z = torch.FloatTensor(batch_size, z_dim).uniform_(0.0,1.0)
+    if cudev >= 0:
+        z = torch.cuda.FloatTensor(batch_size, z_dim).normal_(0.0,1.0)
+    else:
+        z = torch.FloatTensor(batch_size, z_dim).normal_(0.0,1.0)
+    return z
 
 def main(args):
     cfg = vars(args)
@@ -123,10 +151,11 @@ def main(args):
     if cudev >= 0:
         m_gen.cuda(cudev)
         m_disc.cuda(cudev)
-    optimizer = torch.optim.SGD([{"params" : m_gen.parameters()},
-        {"params" : m_disc.parameters()}], lr=cfg["lr"],
+    optG = torch.optim.SGD([{"params" : m_gen.parameters()}], lr=cfg["lr_g"],
         momentum=cfg["momentum"])
-    train(m_gen, m_disc, train_loader, test_loader, optimizer, cfg)
+    optD = torch.optim.SGD([{"params" : m_disc.parameters()}], lr=cfg["lr_d"],
+        momentum=cfg["momentum"])
+    train(m_gen, m_disc, train_loader, test_loader, (optD,optG), cfg)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -137,7 +166,9 @@ if __name__ == "__main__":
     parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument("--z-dim", type=int, default=10,
         help="Number of latent space units")
-    parser.add_argument("--lr", type=float, default=0.0001,
+    parser.add_argument("--lr-d", type=float, default=0.0001,
+            help="Model learning rate")
+    parser.add_argument("--lr-g", type=float, default=0.001,
             help="Model learning rate")
     parser.add_argument("--momentum", type=float, default=0.9,
             help="Momentum parameter for the SGD optimizer")
---
+++ utils.py
@@ -0,0 +1,4 @@
+"""
+General utilities for working with the code for Packt Fundamentals of GANs
+"""
+
